ca0c5e8713351bfd3d6ff45b6d037fb187f25389
AJ-Furd <aj.furedy@uleth.ca>
2016-04-19 14:17:04 -0600

    Added files via upload

---
+++ JavaScriptPractice/JavaScript/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function  () {
+	console.log("hello");
+$('div').text("Hey there.");
+
+});
+
+
+
+
+	// body...
+
+
+//window.onload = function(){
+
+//document.getElementById("calculate").onclick = calculatePledge;
+
+//function calculatePledge() {
+//var num1 = document.getElementById("value1").value;
+//var num2 = document.getElementById("value2").value;
+
+//var result = num1 * num2;
+
+//var text;
+
+//if (result > 1000){
+//	text = "Hey big spender";
+//}
+
+//else if (result > 100){
+//	text = "Thanks bruh";
+//}
+
+
+//else {
+//	text = "Cheap";
+//}
+//	var happy = ":D";
+//	var sad = ":(";
+//	var angry = ":V";
+//	var mood = document.getElementById("mood");
+
+//	document.getElementById("calculation").innerHTML = "$" + result + "(" + text +")";
+//		mood.innerHTML = happy;
+
+//	}
+
+//}
+//}
